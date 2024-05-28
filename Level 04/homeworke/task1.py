27b885b930834aff7f4816ccf9b94f0b2ef361ff
saba1518 <sabacaficadze@gmail.com>
2024-05-27 17:13:57 -0700

    commit

--- Level 04/homeworke/task1.py
+++
@@ -1,1 +0,0 @@
-print("გადმოვწერე და გავაკეტე sing up sololerns")
---
+++ Level 05/HomeWork/task1.py
@@ -0,0 +1,21 @@
+name = ("saba")
+
+age = (13)
+
+nickname = ("sabiasi")
+
+height = (167)
+
+favorite_food = ("Pizza")
+
+Address = (15-10)
+
+favorite_car = ("BMW")
+
+print(name)
+print(age)
+print(nickname)
+print(height)
+print(favorite_food)
+print(Address)
+print(favorite_car)
