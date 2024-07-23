cdfab8d233989d9c34a15096d03536f891677ff1
saba1518 <sabacaficadze@gmail.com>
2024-07-22 17:32:59 -0700

    commit

---
+++ Level 17/homework/task1.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter number "))
+
+num2 = int(input("enter number 1"))
+
+
+print(num1 < 1000 and num1 == num2)
---
+++ Level 17/homework/task2.py
@@ -0,0 +1,5 @@
+num1 = int(input("enter the leson english "))
+
+num2 = int(input("enter the leson math "))
+
+print(num1 + num2)
